11f85d302a07b949b8dde4d8fb5598a0c40011f4
北山_Besson <137259636+BeiShanair@users.noreply.github.com>
2024-03-14 10:15:33 +0800

    study day3 update

--- day3/t5.py
+++ day3/t5.py
@@ -1,9 +1,9 @@
 money = input("请输入金额：")
-type = money[0:3]
-if type == "RMB":
+money_type = money[0:3]
+if money_type == "RMB":
     money = eval(money[3:])
     print("USD", money / 6.78)
-elif type == "USD":
+elif money_type == "USD":
     money = eval(money[3:])
     print("RMB", money * 6.78)
 else:
