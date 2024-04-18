903df94f72c178f47e4b4ff0509ea5e225ec5d0f
北山_Besson <137259636+BeiShanair@users.noreply.github.com>
2024-04-18 10:10:42 +0800

    study part5 update

--- part5/t4.py
+++ part5/t4.py
@@ -3,10 +3,12 @@ a = []
 for i in range(20):
     a.append(random.randint(1000, 5000))
 
-print(a)
+print("生成的列表：", a)
 su_shu = (2, 3, 5, 7)
-for i in su_shu:
-    for j in a:
-        if j % i != 0:
-            print(j, end=" ")
+print("不能被10以内的素数整除的元素：", end="")
+for i in a:
+    for j in su_shu:
+        if i % j == 0:
             break
+    else:
+        print(i, end=" ")
