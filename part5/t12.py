cb1ee0dd57c2fece5a2a11c37ae8fa7bf0e273bb
北山_Besson <137259636+BeiShanair@users.noreply.github.com>
2024-04-18 10:23:36 +0800

    study part5 update

--- part5/t12.py
+++ part5/t12.py
@@ -3,10 +3,10 @@ list1 = []
 a = 0
 for i in range(50):
     list1.append(random.randint(0, 10))
-    print(list1[i], end=" ")
     if a % 10 == 0 and a != 0:
         print()
     a += 1
+    print("{:^5}".format(list1[i]), end=" ")
 
 print()
 
